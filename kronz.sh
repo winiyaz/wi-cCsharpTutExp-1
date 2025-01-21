04471bbb69b8dbee502d015b387cfaee0389e81e
winiyaz <winiyaz@proton.me>
2025-01-21 17:51:58 +0000

    🫦

---
+++ kronz.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Function to execute every minute
+minute_task() {
+    while true; do
+        # Get the current timestamp
+        timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
+        # Perform your desired task here (e.g., check a log file, update a database)
+        echo "[$timestamp] Minute task executed " >>time.txt
+        ./g
+
+        sleep 200 # Sleep for 60 seconds (1 minute)
+    done
+}
+minute_task
